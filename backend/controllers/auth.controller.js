73b3f3b5b20c45ccdb617eac8b9f564cc130b82f
dnnguyen35 <dnnguyeen2003@gmail.com>
2026-03-23 14:40:19 +0700

    fix(auth): fix error in signup feature

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -56,6 +56,7 @@ const signup = async (req, res) => {
       .status(200)
       .json({ message: "OTP has beeen send to email", otpExpireAt });
   } catch (error) {
+    console.error("Error in signup:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
